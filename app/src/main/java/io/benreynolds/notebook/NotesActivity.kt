5966d22e0cecf4f4d1a274daba87c74a6ecd28d1
Ben Reynolds <breynolds1994@outlook.com>
2018-09-11 22:09:03 +0100

    Rename MainActivity to NotesActivity

--- app/src/main/java/io/benreynolds/notebook/NotesActivity.kt
+++ app/src/main/java/io/benreynolds/notebook/NotesActivity.kt
@@ -3,7 +3,7 @@ package io.benreynolds.notebook
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
